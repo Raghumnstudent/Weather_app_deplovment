4acc3abbc63bad3b3bc047d1b2d92ed1aa7c4ae6
raghumn <raghumn688@gmail.com>
2021-12-26 17:51:42 +0530

    raghu

---
+++ app1.js
@@ -0,0 +1,11 @@
+//accessing geocode function
+const geocode = require("./geocode");
+
+geocode('Mumbai',(error,data)=>{
+    if(error){
+        console.log("Error ",error)
+    }
+    console.log("Latitude: "+data.latitude);
+    console.log("longitude: "+data.longitude);
+    console.log("Location: "+data.location);   
+})
